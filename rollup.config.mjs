4d15ec404b786bb5369e4b48e4984397c772e535
Fyodor Shklyar <30theflame05@gmail.com>
2024-01-23 14:37:28 +0300

    initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,19 @@
+import typescript from "rollup-plugin-typescript2";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/bundle.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/bundle.min.js",
+      format: "cjs",
+      name: "version",
+      plugins: [terser()],
+    },
+  ],
+  plugins: [typescript()],
+};
