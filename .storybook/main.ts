945e2866c36933f77a9ff224236d2cb01b03c743
Fyodor Shklyar <30theflame05@gmail.com>
2024-02-02 13:04:34 +0300

    SETTINGS: eslint import/resolver, prettierignore for .eslintrc

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,16 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from "node:path";
+import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: "@storybook/react-webpack5",
     options: {
       builder: {
         useSWC: true,
@@ -17,7 +18,18 @@ const config: StorybookConfig = {
     },
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config?.resolve?.alias,
+        "@constants": path.resolve(__dirname, "../src/constants"),
+      },
+    };
+
+    return config;
   },
 };
 export default config;
