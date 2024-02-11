8e9bf3d37e4d0b8210d6329222383876a5f23d0b
Fyodor Shklyar <30theflame05@gmail.com>
2024-02-11 23:59:46 +0300

    FEATURE: add exports in index.ts for using library

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as CalendarManager } from "decorators/ClaendarManager";
+export { default as withHolidays } from "decorators/withHolidays";
+export { default as withSundayFirst } from "decorators/withSundayFirst";
+export { default as withTodos } from "decorators/withTodos";
+export { default as withWeekends } from "decorators/withWeekends";
+export { default as DatePicker } from "components/DatePicker/DatePicker";
+export { default as RangePicker } from "components/RangePicker/RangePicker";
