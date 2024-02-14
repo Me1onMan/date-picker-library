f8856fd3b88fd67572ad8242988306381efe9977
Fyodor Shklyar <30theflame05@gmail.com>
2024-02-14 09:59:06 +0300

    FIX: update build

---
+++ build/components/ErrorBoundary/ErrorBoundary.d.ts
@@ -0,0 +1,4 @@
+import React from "react";
+import { ErrorBoundaryProps } from "./interfaces";
+declare const _default: React.FC<ErrorBoundaryProps>;
+export default _default;
--- build/index.d.ts
+++ build/index.d.ts
@@ -1,5 +1,7 @@
-/// <reference types="react" />
-declare const _default: {
-    TestComponent: import("react").FC<import("./components/TestComponent/TestComponent.types").TestComponentProps>;
-};
-export default _default;
+export { default as CalendarManager } from "decorators/ClaendarManager";
+export { default as withHolidays } from "decorators/withHolidays";
+export { default as withSundayFirst } from "decorators/withSundayFirst";
+export { default as withTodos } from "decorators/withTodos";
+export { default as withWeekends } from "decorators/withWeekends";
+export { default as DatePicker } from "components/DatePicker/DatePicker";
+export { default as RangePicker } from "components/RangePicker/RangePicker";
